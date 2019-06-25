d2869f751f1ceaa7a7c4fd56bdddbc153b1e511c
Andrew Tiller <tiller.andrew@gmail.com>
2019-06-25 18:47:25 -0500

    structure of three columns is made

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,16 @@
+// on the right
+import React from 'react';
+
+import './Orders.scss';
+
+class Orders extends React.Component {
+  render() {
+    return (
+      <div className="Orders">
+        <h2>Orders</h2>
+      </div>
+    );
+  }
+}
+
+export default Orders;
