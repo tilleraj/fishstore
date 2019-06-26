67833454743ddaea283aed2fe952207b74d26389
Andrew Tiller <tiller.andrew@gmail.com>
2019-06-25 20:10:48 -0500

    added axios call and fishData file

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,20 @@
 // on the left
 import React from 'react';
+import fishData from '../../helpers/data/fishData';
 
 import './Inventory.scss';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(error => console.error('could not get fishes', error));
+  }
+
   render() {
     return (
       <div className="Inventory">
