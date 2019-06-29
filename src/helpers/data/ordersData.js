5ff7f13621fa9003bb4d48241858c1963b7bb5b4
Andrew Tiller <tiller.andrew@gmail.com>
2019-06-29 12:40:18 -0500

    calculating fish numbers, nice looking dates, and getting ordername

---
+++ src/helpers/data/ordersData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
+
+const getMyOrders = uid => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/orders.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      const orders = [];
+      Object.keys(response.data).forEach((fbKey) => {
+        response.data[fbKey].id = fbKey;
+        orders.push(response.data[fbKey]);
+      });
+      resolve(orders);
+    })
+    .catch(error => reject(error));
+});
+
+export default { getMyOrders };
